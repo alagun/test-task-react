55b13edf8c6aa20516154cf3930a9e37d8870bf3
Alexey Lagun <alexey.lagun@gmail.com>
2023-05-28 12:07:34 +0300

    feat: add dropdown

--- src/components/Group.js
+++ src/components/Group.js
@@ -33,6 +33,50 @@ export default function Group({ title, subtitle, obj }) {
         <span>{subtitle}</span>
       </div>
       <div className="stats">{information}</div>
+      <div className="description">
+        <div className="description-item">
+          <h3 className="desritpion-title">{obj[0].title}</h3>
+          <span className="desritpion-text">
+            {
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet suscipit cupiditate exercitationem blanditiis. Commodi, impedit sint eaque quibusdam quis debitis consectetur nostrum soluta sequi odit. Quas dicta vero dignissimos quo!"
+            }
+          </span>
+          <br />
+          <span className="desritpion-text">
+            {
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet suscipit cupiditate exercitationem blanditiis. Commodi, impedit sint eaque quibusdam quis debitis consectetur nostrum soluta sequi odit. Quas dicta vero dignissimos quo!"
+            }
+          </span>
+        </div>
+        <div className="description-item">
+          <h3 className="desritpion-title">{obj[1].title}</h3>
+          <span className="desritpion-text">
+            {
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet suscipit cupiditate exercitationem blanditiis. Commodi, impedit sint eaque quibusdam quis debitis consectetur nostrum soluta sequi odit. Quas dicta vero dignissimos quo!"
+            }
+          </span>
+          <br />
+          <span className="desritpion-text">
+            {
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet suscipit cupiditate exercitationem blanditiis. Commodi, impedit sint eaque quibusdam quis debitis consectetur nostrum soluta sequi odit. Quas dicta vero dignissimos quo!"
+            }
+          </span>
+        </div>
+        <div className="description-item">
+          <h3 className="desritpion-title">{obj[2].title}</h3>
+          <span className="desritpion-text">
+            {
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet suscipit cupiditate exercitationem blanditiis. Commodi, impedit sint eaque quibusdam quis debitis consectetur nostrum soluta sequi odit. Quas dicta vero dignissimos quo!"
+            }
+          </span>
+          <br />
+          <span className="desritpion-text">
+            {
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet suscipit cupiditate exercitationem blanditiis. Commodi, impedit sint eaque quibusdam quis debitis consectetur nostrum soluta sequi odit. Quas dicta vero dignissimos quo!"
+            }
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
